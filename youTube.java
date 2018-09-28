b225b3fefadb7a1dacd0ea3d13b6d928efdaeff2
omsoo <onyebuchisundayo@gmail.com>
2018-09-28 12:29:01 +0100

    my first java programm..

---
+++ youTube.java
@@ -0,0 +1,8 @@
+// My first Java code
+
+class apples{
+	public static void main(String args[]){
+		System.out.println("Hello youtube");
+		System.out.print("I am currently using the command prompt to run my Java code");
+	}
+}
